091b8f1e8a15fe6a3fb6457e4b55ad83a70cd755
El-Mongo-Bongo <genebichler@hotmail.com>
2021-04-10 15:21:57 +0200

    first steps in dash

--- DGUI-main/index.py
+++ DGUI-main/index.py
@@ -1,5 +1,21 @@
-# python file
+# Run with python index.py and
+# visit http://127.0.0.1:8050/ browser
 
-print("Hello World")
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
 
-print("Hallo World 2")
+# creat app
+app = dash.Dash()
+
+# html output
+app.layout = html.Div (children=[
+    html.H1(children='Hello World'),
+    html.P(children='Lorem Ipsum')
+])
+
+# on production server change debug to false
+if __name__ == '__main__':
+    app.run_server(debug=True)
