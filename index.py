1b63527f4cf335faa89207ab2905d894a68f1aa0
El-Mongo-Bongo <genebichler@hotmail.com>
2021-04-24 13:37:47 +0200

    Aufgabe 11a)

--- index.py
+++ index.py
@@ -1,21 +1,21 @@
 # Run with python index.py and
 # visit http://127.0.0.1:8050/ browser
 
+import pandas as pd
+import plotly.express as px
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import pandas as pd
+from dash.dependencies import Input, Output
 
-# creat app
-app = dash.Dash()
 
-# html output
-app.layout = html.Div (children=[
-    html.H1(children='Hello World'),
-    html.P(children='Lorem Ipsum')
-])
+#Aufgabe 11a) Einlesen des Covid-19 Datensatzes mit einem Pandas DataFrame
+df = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
 
-# on production server change debug to false
-if __name__ == '__main__':
-    app.run_server(debug=True)
+
+#Aufgabe 11b) Berechnen von Mittelwert, Standardabweichung, Varianz, Median und Maximum für das Attribut «new_cases»
+print(df[['new_cases']])
+
+
+#Aufgabe 12c) Land mit dem höchsten Mittelwert beim Attribut «new_cases» 
